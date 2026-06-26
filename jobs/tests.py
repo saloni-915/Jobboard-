7765e86e8f3bf1a44fde0e5b31fe25d5722d9bf3
Saloni <salonisain915@gmail.com>
2026-06-26 16:28:12 +0530

    test file updated

--- jobs/tests.py
+++ jobs/tests.py
@@ -16,5 +16,5 @@ def test_homepage_loads():
 @pytest.mark.django_db
 def test_admin_login_page():
     client = Client()
-    response = client.get("/admin/")
+    response = client.get("/admin/login/")
     assert response.status_code == 200
